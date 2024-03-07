1941528760d95074fb98d05360ab434fbc9b6a33
vini3055 <kvineethvini94@gmail.com>
2024-03-07 14:05:50 +0530

    Subscription added, checks if same content is entered or not.

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,6 +1,7 @@
 import { PostService } from './../post.service';
-import { Component, Injectable, Input, OnInit } from '@angular/core';
+import { Component, Injectable, Input, OnDestroy, OnInit } from '@angular/core';
 import { Post } from '../post.model';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -8,11 +9,21 @@ import { Post } from '../post.model';
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
+  private postSubscription = new Subscription;
   posts: Post[] = [];
+
   constructor(public postService: PostService) { }
+
   ngOnInit() {
     this.posts = this.postService.getPosts();
+    this.postSubscription = this.postService.getPostUpdateListner().subscribe((post: Post[]) => {
+      this.posts = post;
+    })
+  }
+
+  ngOnDestroy() {
+    this.postSubscription.unsubscribe()
   }
 
 }
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
+import { Subject } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class PostService {
     private Posts: Post[] = [];
 
+    private postUpdated = new Subject<Post[]>();
+
     getPosts() {
         return this.Posts
     }
+
+    getPostUpdateListner() {
+        return this.postUpdated.asObservable();
+    }
+
     addPosts(title: string, content: string) {
+        // checks if the content / title is same or not
+        if (this.Posts.find((x) => x.title == title || x.content == content)) {
+            return alert('already existed');
+        }
         const post: Post = { title: title, content: content }
         this.Posts.push(post);
+        this.postUpdated.next([...this.Posts]);
     }
+
 }
