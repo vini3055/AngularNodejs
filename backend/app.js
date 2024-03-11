c51399f89b544d80f7d78386d7ccadfd2c2003d7
vini3055 <kvineethvini94@gmail.com>
2024-03-11 10:43:45 +0530

    setting a dummy json having a api set, some server configuration settings

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,51 @@ const express =require('express');
 
 const app = express();
 
-// parse incoming requests
-app.use((req, res, next)=>{
-    console.log('first middle ware1',req.url);
-    next();
-})
+// // parse incoming requests
+// app.use((req, res, next)=>{
+//     console.log('first middle ware1',req.url);
+//     next();
+// })
 
-app.use((req, res, next)=>{
-res.send('hello from Express!')
-//  res.end();
-})
+// app.use((req, res, next)=>{
+// res.send('hello from Express!')
+// //  res.end();
+// })
 
+
+app.use('/api/posts',(req, res, next)=>{
+    const post =[
+        {
+          "id": 1,
+          "title": "Introduction to JSON",
+          "content": "JSON (JavaScript Object Notation) is a lightweight data-interchange format."
+        },
+        {
+          "id": 2,
+          "title": "Benefits of JSON",
+          "content": "JSON is easy for humans to read and write. It is also easy for machines to parse and generate."
+        },
+        {
+          "id": 3,
+          "title": "JSON Syntax",
+          "content": "JSON syntax is based on JavaScript object notation syntax, but it is not tied to JavaScript exclusively."
+        },
+        {
+          "id": 4,
+          "title": "Usage of JSON",
+          "content": "JSON is commonly used for transmitting data in web applications (e.g., sending data from the server to the client, or vice versa)."
+        },
+        {
+          "id": 5,
+          "title": "Parsing JSON",
+          "content": "In most programming languages, libraries exist to parse JSON into native data structures."
+        }
+      ]
+      
+    res.status(200).json({
+        // message:'post successful',
+        post
+    })
+})
 
 module.exports = app;
